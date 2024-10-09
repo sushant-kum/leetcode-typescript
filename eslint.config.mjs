85879bf88e4bc72706a8285ccc4e82627f2d0c7c
Sushant Kumar <sushant.kum96@gmail.com>
2024-10-09 13:40:16 +0530

    chore: added import-order eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,50 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-// import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { fixupPluginRules } from "@eslint/compat";
+import pluginJs from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
+import importRecommended from "eslint-plugin-import/config/recommended.js";
+import jsdoc from "eslint-plugin-jsdoc";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tsEslint from "typescript-eslint";
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.{js,ts}"], languageOptions: { sourceType: "script" } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tsEslint.configs.recommended,
   eslintPluginPrettierRecommended,
+  jsdoc.configs["flat/recommended"],
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      import: fixupPluginRules(importPlugin),
+    },
+    settings: {
+      "import/resolver": {
+        node: true,
+        typescript: true,
+      },
+    },
+    rules: {
+      ...importRecommended.rules,
+      "import/no-unresolved": "error",
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "parent", "sibling", "index", "object", "type"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
 ];
-
-// {
-//   "root": true,
-//   "parser": "@typescript-eslint/parser",
-//   "plugins": ["@typescript-eslint", "prettier"],
-//   "extends": [
-//     "eslint:recommended",
-//     "plugin:@typescript-eslint/eslint-recommended",
-//     "plugin:@typescript-eslint/recommended",
-//     "prettier",
-//   ],
-//   "env": {
-//     "node": true,
-//   },
-//   "rules": {
-//     "prettier/prettier": "error",
-//   },
-// }
